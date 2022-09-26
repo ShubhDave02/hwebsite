9fb1e183ef04be7b02ac439bbd9182ab3dbcddb9
ShubhDave02 <99464905+ShubhDave02@users.noreply.github.com>
2022-09-26 14:18:18 +0530

    Add files via upload

---
+++ Hwebsite/main.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dekhle Dekhle</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+  <div class="container">
+    <div class="heading">
+      <h3> 2020-2021</h3>
+    </div>
+    <div class="box">
+      
+      <div class="dream">
+        <img src="IMG/MONDLER11.1.jpg">
+        <img src="IMG/Mondler15.png">
+        <img src="IMG/MONDLER14.jpg">  
+        
+      </div>
+      <div class="dream">
+        <img src="IMG/MONDLER11.1.jpg">
+        <img src="IMG/Mondler15.png">
+        <img src="IMG/MONDLER14.jpg">
+         
+      </div>
+      <div class="dream">
+      <img src="IMG/MONDLER11.1.jpg">
+      <img src="IMG/Mondler15.png">
+      <img src="IMG/MONDLER2.jpg">
+      <img src="IMG/Mondler15.png">
+      </div>
+    </div>
+    
+    
+    
+    
+    <div class="container">
+      <div class="heading">
+        <h3> 2021-2022</h3>
+      </div>
+      <div class="box">
+        
+        <div class="dream">
+          <img src="IMG/MONDLER11.1.jpg">
+           <img src="IMG/Mondler15.png">
+            <img src="IMG/MONDLER14.jpg">  
+            
+        </div>
+        <div class="dream">
+          <img src="IMG/MONDLER11.1.jpg">
+            <img src="IMG/Mondler15.png">
+               <img src="IMG/MONDLER14.jpg">
+            
+        </div>
+        <div class="dream">
+        <img src="IMG/MONDLER11.1.jpg">
+        <img src="IMG/Mondler15.png">
+          <img src="IMG/MONDLER2.jpg">
+           <img src="IMG/MONDLER7.jpg">
+        </div>
+      </div>
+
+    <div class="btn">
+      <a href="#"> More </a>
+    </div>
+
+
+
+
+
+  </div>
+
+</body>
+</html>
